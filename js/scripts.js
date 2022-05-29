fc5253df4d12b134fb635fa7c0a0e9db9def61de
Yesen Kandalama <70659937+yesenvidnath@users.noreply.github.com>
2022-05-29 16:48:04 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+// Nav menue
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
+
+//Random quote jenarator Js code
+const text = document.getElementById("quote");
+const author = document.getElementById("author");
+const tweetButton = document.getElementById("tweet");
+const facebookButton = document.getElementById("facebook");
+
+const getNewQuote = async () => {
+  //api for quotes
+  var url = "https://type.fit/api/quotes";
+
+  // fetch the data from api
+  const response = await fetch(url);
+  console.log(typeof response);
+  //convert response to json and store it in quotes array
+  const allQuotes = await response.json();
+
+  // Generates a random number between 0 and the length of the quotes array
+  const indx = Math.floor(Math.random() * allQuotes.length);
+
+  //Store the quote present at the randomly generated index
+  const quote = allQuotes[indx].text;
+
+  //Store the author of the respective quote
+  const auth = allQuotes[indx].author;
+
+  if (auth == null) {
+    author = "Anonymous";
+  }
+
+  //function to dynamically display the quote and the author
+  text.innerHTML = quote;
+  author.innerHTML = "~ " + auth + " ~";
+
+  //tweet the quote
+  tweetButton.href ="https://twitter.com/intent/tweet?text=" + quote + " ~ " + auth;
+  facebookButton.href ="https://twitter.com/intent/tweet?text=" + quote + " ~ " + auth;
+};
+
+getNewQuote();
